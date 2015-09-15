5257366a464c193545697a99fd7644fed95b3a4b
Matthew <mwflickner@gmail.com>
2015-09-15 14:08:04 -0700

    IBOutlets for create account vc linked

--- clients/iOS/Grapevine/Grapevine/CreateAccountViewController.swift
+++ clients/iOS/Grapevine/Grapevine/CreateAccountViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class CreateAccountViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    @IBOutlet weak var emailAddressTextField: UITextField!
+    
+    @IBOutlet weak var createAccountButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
