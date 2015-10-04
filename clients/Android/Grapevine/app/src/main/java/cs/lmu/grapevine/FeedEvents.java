78ab165c42b56519f19401669082a8739e07b53e
Jeff Fennell <jfennell@lion.lmu.edu>
2015-10-04 10:43:49 -0700

    Add event request to feed activity.

--- clients/Android/Grapevine/app/src/main/java/cs/lmu/grapevine/FeedEvents.java
+++ clients/Android/Grapevine/app/src/main/java/cs/lmu/grapevine/FeedEvents.java
@@ -6,6 +6,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FeedEvents extends AppCompatActivity {
 
@@ -13,12 +19,33 @@ public class FeedEvents extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_events);
-        Intent loginIntent = getIntent();
-        String jwt = loginIntent.getStringExtra("JWT");
 
-        //print out the jwt received from the login response
-        TextView eventFeed = (TextView)(findViewById(R.id.event_feed));
-        eventFeed.setText(jwt);
+        String eventRequestURL = MainActivity.apiURL + "/api/v1/events";
+        StringRequest userLoginRequest = new StringRequest(Request.Method.GET,
+            eventRequestURL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                TextView eventFeed = (TextView)findViewById(R.id.event_feed);
+                eventFeed.setText(response.toString());
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                TextView eventFeed = (TextView)findViewById(R.id.event_feed);
+                eventFeed.setText(R.string.event_request_error);
+
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders(){
+                HashMap<String, String> eventRequestHeaders = new HashMap<>();
+                eventRequestHeaders.put("x-access-token", MainActivity.authenticationToken);
+                eventRequestHeaders.put("x-key", "jeff");
+                return eventRequestHeaders;
+            }
+        };
+        MainActivity.httpRequestQueue.add(userLoginRequest);
     }
 
     @Override
