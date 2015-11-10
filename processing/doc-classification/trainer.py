6c12a289c11af5d942d8a2603a046cbdd78e6cd4
Cameron Billingham <cbillin3@lion.lmu.edu>
2015-11-10 09:39:22 -0800

    pulling from testdata.csv and parsing words into classified set

--- processing/doc-classification/trainer.py
+++ processing/doc-classification/trainer.py
@@ -1,7 +1,37 @@
 import nltk
 import csv
+from nltk import word_tokenize
+from nltk.corpus import stopwords
 
-testData = list( csv.reader( open("testdata.csv") ) )
+stop_words = set(stopwords.words('english'))
 
-for row in testData:
-   print row
+
+testDataArray = list( csv.reader( open("testdata.csv") ) )
+headers = testDataArray.pop(0)
+labelHeaders = headers[1:]
+
+
+testData = []
+for row in testDataArray:
+   boolRow = [row.pop(0)]
+   for label in row:
+      boolRow.append( bool(int(label)) )
+   testData.append( dict(zip(headers, boolRow)))
+
+
+classified_set = []
+for classifiedPost in testData:
+   post = nltk.word_tokenize( classifiedPost["post"] )
+   for word in post:
+      if word in stop_words:
+         post.remove(word)
+   for header in labelHeaders:
+      if classifiedPost[header]:
+         classified_set.append( (post, header) )
+
+
+print classified_set[0]
+
+# classifier = nltk.NaiveBayesClassifier.train(training_set)
+# accuracy = nltk.classify.accuracy(classifier, testing_set)
+# print 
