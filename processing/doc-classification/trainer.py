190268ae48d34e0e133cf69f3e034b594bffa4ec
Cameron Billingham <cbillin3@lion.lmu.edu>
2015-11-10 11:35:54 -0800

    added support for custom stop-words

--- processing/doc-classification/trainer.py
+++ processing/doc-classification/trainer.py
@@ -5,7 +5,7 @@ from nltk import word_tokenize
 from nltk.corpus import stopwords
 
 stop_words = set(stopwords.words('english'))
-
+stop_words.update( set(line.strip() for line in open('customstopwords.txt')) )
 
 testDataArray = list( csv.reader( open("testdata.csv") ) )
 headers = testDataArray.pop(0)
@@ -24,16 +24,17 @@ classified_set = []
 for classifiedPost in testData:
 
    post = nltk.word_tokenize( classifiedPost["post"] )
+   post = [word.lower() for word in post]
    for word in post:
       if word in stop_words:
-         post.remove(word)
+         post = filter(lambda a: a != word, post)
 
    for label in labelHeaders:
       if classifiedPost[label]:
          classified_set.append( (post, label) )
 
 
-print classified_set[0]
+print classified_set
 
 # classifier = nltk.NaiveBayesClassifier.train(training_set)
 # accuracy = nltk.classify.accuracy(classifier, testing_set)
