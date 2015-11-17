98242e6d4af8067b2a2840ba5284918b92b6c7c0
Matthew <mwflickner@gmail.com>
2015-11-16 21:22:06 -0800

    added short month form method

--- clients/iOS/Grapevine/Grapevine/HelperMethods.swift
+++ clients/iOS/Grapevine/Grapevine/HelperMethods.swift
@@ -57,6 +57,37 @@ func monthIntToMonthString(date: CVDate) -> String {
     }
 }
 
+func monthIntToShortMonthString(month: Int) -> String {
+    switch month {
+    case 1:
+        return "JAN"
+    case 2:
+        return "FEB"
+    case 3:
+        return "MAR"
+    case 4:
+        return "APR"
+    case 5:
+        return "MAY"
+    case 6:
+        return "JUN"
+    case 7:
+        return "JUL"
+    case 8:
+        return "AUG"
+    case 9:
+        return "SEP"
+    case 10:
+        return "OCT"
+    case 11:
+        return "NOV"
+    case 12:
+        return "DEC"
+    default:
+        return "Not a month"
+    }
+}
+
 func sameDate(date1: CVDate, date2: CVDate) -> Bool {
     if ((date1.month == date2.month) && (date2.day == date1.day) && (date1.year == date2.year)) {
         return true
