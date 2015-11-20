a2c8435cc00d36f2b7bab1bb769948f143773b1b
Jeff Fennell <jfennell@lion.lmu.edu>
2015-11-19 22:25:12 -0800

    Add spinner for login feedback.

--- clients/Android/Grapevine/app/src/main/java/cs/lmu/grapevine/requests/LoginRequest.java
+++ clients/Android/Grapevine/app/src/main/java/cs/lmu/grapevine/requests/LoginRequest.java
@@ -1,8 +1,11 @@
 package cs.lmu.grapevine.requests;
 
 import android.app.Activity;
+import android.view.View;
+import android.widget.ProgressBar;
 import com.android.volley.toolbox.JsonObjectRequest;
 import cs.lmu.grapevine.BuildConfig;
+import cs.lmu.grapevine.R;
 import cs.lmu.grapevine.requests.listeners.error.LoginErrorListener;
 import cs.lmu.grapevine.requests.listeners.success.LoginSuccessListener;
 
@@ -11,14 +14,22 @@ import cs.lmu.grapevine.requests.listeners.success.LoginSuccessListener;
  */
 public class LoginRequest extends JsonObjectRequest {
     private static String loginUrl = BuildConfig.API_HOST + "login";
+    private Activity parentActivity;
 
     public LoginRequest(Activity parentActivity, String requestBodyString) {
         super(Method.POST,
-              loginUrl,
-              RequestHelper.serializeRequestBody(requestBodyString),
-              new LoginSuccessListener(parentActivity),
-              new LoginErrorListener(parentActivity)
+                loginUrl,
+                RequestHelper.serializeRequestBody(requestBodyString),
+                new LoginSuccessListener(parentActivity),
+                new LoginErrorListener(parentActivity)
         );
+        this.parentActivity = parentActivity;
+        showLoginSpinner();
+    }
+
+    private void showLoginSpinner() {
+        ProgressBar loginProgress = (ProgressBar)parentActivity.findViewById(R.id.login_progress);
+        loginProgress.setVisibility(View.VISIBLE);
     }
 
 }
