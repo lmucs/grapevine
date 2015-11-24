ad587ce252e0a1159715157ec837344219ac37f5
Jeff Fennell <jfennell@lion.lmu.edu>
2015-11-23 21:18:22 -0800

    Implement follow feed feature.

--- clients/Android/Grapevine/app/src/main/java/cs/lmu/grapevine/activities/FollowFeed.java
+++ clients/Android/Grapevine/app/src/main/java/cs/lmu/grapevine/activities/FollowFeed.java
@@ -32,7 +32,7 @@ public class FollowFeed extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_add_group, menu);
+        getMenuInflater().inflate(R.menu.menu_follow_feed, menu);
         return true;
     }
 
