0e466ddedd4e2b36eb244533df92284d10f01d0a
Cameron Billingham <cbillin3@lion.lmu.edu>
2015-12-05 14:51:43 -0800

    ran and dumped classifier into pickle

--- processing/doc-classification/scikit-trainer.py
+++ processing/doc-classification/scikit-trainer.py
@@ -3,6 +3,7 @@ import nltk
 import csv
 import pickle
 import random
+import re
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from sklearn.pipeline import Pipeline
@@ -14,14 +15,15 @@ from sklearn.multiclass import OneVsRestClassifier
 stop_words = set(stopwords.words('english'))
 stop_words.update( set(line.strip() for line in open('customstopwords.txt')) )
 
-test_data_array = list( csv.reader( open("testdata.csv") ) )
+test_data_array = list( csv.reader( open("LMUTwitterFacebookCorpus.csv") ) )
 headers = test_data_array.pop(0)
 label_headers = headers[1:]
 
 random.shuffle(test_data_array)
+train_length = len(test_data_array) * .9
+
 posts = []
 binary_labels = []
-
 for row in test_data_array:
    posts.append( (row.pop(0)).decode("string_escape") )
    labels = []
@@ -29,12 +31,12 @@ for row in test_data_array:
       labels.append( int(label) )
    binary_labels.append( labels )
 
-def process(string) :
+def process(string):
    words = []
-   split_string = string.split(' ')
+   split_string = string.split()
    for string in split_string:
-      if string.startswith(("http","&amp")):
-         continue
+      if bool(re.search(r'\d', string)) or "http" in string or "&amp" in string or "www" in string:
+            continue
       string = string.decode("unicode_escape")
       string = nltk.word_tokenize(string)
       string = [s.lower() for s in string]
@@ -49,29 +51,23 @@ posts = [process(post) for post in posts]
 posts = np.array(posts)
 binary_labels = np.array(binary_labels)
 
-NBclassifier = Pipeline([
-    ('vectorizer', TfidfVectorizer()),
-    ('clf', OneVsRestClassifier(MultinomialNB()))])
-NBclassifier.fit(posts[:10], binary_labels[:10])
-predicted = NBclassifier.predict(posts[10:])
-score = NBclassifier.score(posts[10:], binary_labels[10:])
+posts_train = posts[:train_length]
+binary_labels_train = binary_labels[:train_length]
+posts_test = posts[train_length:]
+binary_labels_test = binary_labels[train_length:]
 
-SVCclassifier = Pipeline([
-    ('vectorizer', TfidfVectorizer()),
-    ('clf', OneVsRestClassifier(LinearSVC()))])
-SVCclassifier.fit(posts[:10], binary_labels[:10])
+def makePipeline(classifier):
+   return Pipeline([
+    ('vectorizer', TfidfVectorizer(max_features=2000)),
+    ('clf', OneVsRestClassifier(classifier))])
 
-for post in posts[10:]:
-   print post
+SVCclassifier = makePipeline(LinearSVC())
+SVCclassifier.fit(posts_train, binary_labels_train)
+score = SVCclassifier.score(posts_test, binary_labels_test)
 
-print "NB Classifier"
-predicted = NBclassifier.predict_proba(posts[10:])
-score = NBclassifier.score(posts[10:], binary_labels[10:])
-print predicted
-print score
+print "SVCclassifier", score
+print SVCclassifier.named_steps["vectorizer"].get_feature_names()
 
-print "SVC Classifier"
-predicted = SVCclassifier.(posts[10:])
-score = SVCclassifier.score(posts[10:], binary_labels[10:])
-print predicted
-print score
+# save_classifier = open("post-classifier.pickle", "wb")
+# pickle.dump(SVCclassifier, save_classifier)
+# save_classifier.close()
