efcc0107a5050623c9102e85b5ea11f00bff28c1
Cameron Billingham <cbillin3@lion.lmu.edu>
2015-12-07 09:48:40 -0800

    added test for global object

--- processing/classifier/hello.py
+++ processing/classifier/hello.py
@@ -1,9 +1,42 @@
-from flask import Flask
+import pickle
+import json
+from flask import Flask, request, Response
+from flask import jsonify
+from functools import wraps
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+#f = open('my_classifier.pickle', 'rb')
+#classifier = pickle.load(f)
+#f.close()
 
-if __name__ == '__main__':
+def check_auth(username, password):
+    return username == 'cameron' and password == 'billingham'
+
+def authenticate():
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+@app.route("/tags", methods=['POST'])
+@requires_auth
+def decipherTags():
+    data = json.loads(request.data)
+    post = data['post']
+    tags = post
+    tags.append(x)
+    return jsonify(tags = tags)
+
+if __name__ == "__main__":
+    app.debug = True
     app.run()
