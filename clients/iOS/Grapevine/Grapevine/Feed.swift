c2c220727c03292c367acd76772245bd848aa7cc
Matthew <mwflickner@gmail.com>
2015-12-07 14:59:16 -0800

    working on getting feeds

---
+++ clients/iOS/Grapevine/Grapevine/Feed.swift
@@ -0,0 +1,27 @@
+//
+//  Feed.swift
+//  Grapevine
+//
+//  Created by Matthew Flickner on 12/7/15.
+//  Copyright © 2015 Grapevine. All rights reserved.
+//
+
+import UIKit
+import ObjectMapper
+
+class Feed: NSObject, Mappable {
+    
+    var networkName: String!
+    var feedName: String!
+    
+    // Required to implement ObjectMapper
+    required init?(_ map: Map){
+        
+    }
+    
+    func mapping(map: Map) {
+        self.networkName <- map["network_name"]
+        self.feedName <- map["feed_name"]
+    }
+    
+}
