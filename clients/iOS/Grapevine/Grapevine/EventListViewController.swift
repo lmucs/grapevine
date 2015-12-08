0fc45178a55d878a5175a548d3adc421bbad74c4
Matthew <mwflickner@gmail.com>
2015-12-07 22:16:31 -0800

    get events since now works

--- clients/iOS/Grapevine/Grapevine/EventListViewController.swift
+++ clients/iOS/Grapevine/Grapevine/EventListViewController.swift
@@ -161,13 +161,13 @@ class EventListViewController: UIViewController, UITableViewDelegate, UITableVie
     }
     
     func getEventsSince(date: NSDate){
-        let getEventsSinceUrl = NSURL(string: apiBaseUrl + "/api/v1/users/" + String(self.userToken.userID!) + "/events/" + String(self.lastUpdated.timeIntervalSince1970 * 1000))
+        let getEventsSinceUrl = NSURL(string: apiBaseUrl + "/api/v1/users/" + String(self.userToken.userID!) + "/events/" + String(Int(self.lastUpdated.timeIntervalSince1970 * 1000)))
         print(getEventsSinceUrl)
         let requestHeader: [String: String] = [
             "Content-Type": "application/json",
             "x-access-token": String(self.userToken.token!)
         ]
-        print("calling for events now swag")
+        print("calling for new events now swag")
         Alamofire.request(.GET, getEventsSinceUrl!, encoding: .JSON, headers: requestHeader)
             .responseJSON { response in
                 debugPrint(response)
