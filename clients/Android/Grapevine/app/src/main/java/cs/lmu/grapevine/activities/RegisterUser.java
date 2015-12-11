ca4fc449100d9665ae22ad037eff5a999291655b
Jeff Fennell <jfennell@lion.lmu.edu>
2015-12-10 22:11:27 -0800

    Remove settings menu option handling.

--- clients/Android/Grapevine/app/src/main/java/cs/lmu/grapevine/activities/RegisterUser.java
+++ clients/Android/Grapevine/app/src/main/java/cs/lmu/grapevine/activities/RegisterUser.java
@@ -31,11 +31,6 @@ public class RegisterUser extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
