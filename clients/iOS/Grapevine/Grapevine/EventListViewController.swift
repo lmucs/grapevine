41a744ad9b8a15285c719b52c89610081bef9e1d
Matthew <mwflickner@gmail.com>
2015-12-11 01:08:05 -0800

    full text logo now header on navigation bar

--- clients/iOS/Grapevine/Grapevine/EventListViewController.swift
+++ clients/iOS/Grapevine/Grapevine/EventListViewController.swift
@@ -25,11 +25,12 @@ class EventListViewController: UIViewController, UITableViewDelegate, UITableVie
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        let logo = UIImage(named: "grapevine-logo-full-words-small.png")
+        let imageView = UIImageView(image:logo)
+        imageView.contentMode = .ScaleAspectFit
+        self.navigationItem.titleView = imageView
+        
     }
 
     override func didReceiveMemoryWarning() {
