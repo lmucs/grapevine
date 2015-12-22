3f81685791fba6a87a1dfed0f2bd8676069f7dd9
Matthew <mwflickner@gmail.com>
2015-12-21 23:33:10 -0800

    logout view created

---
+++ clients/iOS/Grapevine/Grapevine/SettingsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SettingsViewController.swift
+//  Grapevine
+//
+//  Created by Matthew Flickner on 12/21/15.
+//  Copyright © 2015 Grapevine. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    @IBOutlet weak var logoutButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupGrapevineButton(logoutButton)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func logoutPressed(sender: UIButton!){
+        self.navigationController!.pushViewController(storyboard!.instantiateViewControllerWithIdentifier("LoginViewController") as! LoginViewController, animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
