d4f52baf1054925c209a8944379dddc609fece8a
Jeff Fennell <jfennell@lion.lmu.edu>
2016-03-14 22:52:00 -0700

    Remove unused code and clean up.

--- clients/Android/Grapevine/app/src/main/java/cs/lmu/grapevine/DisplayLicenses.java
+++ clients/Android/Grapevine/app/src/main/java/cs/lmu/grapevine/DisplayLicenses.java
@@ -1,11 +1,8 @@
 package cs.lmu.grapevine;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class DisplayLicenses extends AppCompatActivity {
 
@@ -16,14 +13,6 @@ public class DisplayLicenses extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
